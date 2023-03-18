3c0f3c4cbfaebda418b5a1b0e66e7cfb76c0e0ed
GorNalbandyan <gor.ar.nalb@gmail.com>
2023-03-18 18:40:35 +0400

    Name field is created

--- TrainignProject/MyFirstProject.cs
+++ TrainignProject/MyFirstProject.cs
@@ -14,11 +14,9 @@ namespace TrainignProject
         {
             Console.WriteLine("Enter your age please");
             int myAge = int.Parse(Console.ReadLine());
-
             Console.WriteLine("Enter your spouse's age please");  
             int spouseAge = Convert.ToInt32(Console.ReadLine());
             int sum = myAge + spouseAge;
-
             Console.WriteLine($"The sum age of the couple is: {sum}");
         }
     }
